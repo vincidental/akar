bae10af8a939b82944bff240c3aadf2a1267c0d4
Vincentius Theodore <vtheodore7@gmail.com>
2026-03-31 21:57:55 +0000

    Manual code change

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -31,7 +31,7 @@ export default function HeroSection() {
         >
           Your ads are working.{' '}
           <br className="hidden md:block" />
-          But Your website is{' '}
+          But your website is{' '}
           <em className="not-italic text-green-600">losing the leads.</em>
         </motion.h1>
 
